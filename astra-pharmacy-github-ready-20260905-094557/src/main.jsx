23c7129305af1a42a0e8ad2d04b17357d6dba37d
alihaiderrrr1230-wq <alihaiderrrr1230@gmail.com>
2026-09-06 21:19:48 +0300

    Wrap App component with AstraStoreProvider

--- astra-pharmacy-github-ready-20260905-094557/src/main.jsx
+++ astra-pharmacy-github-ready-20260905-094557/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { AstraStoreProvider } from './store/useAstraStore.js';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AstraStoreProvider>
+      <App />
+    </AstraStoreProvider>
   </React.StrictMode>
 );
